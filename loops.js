79fb7e3660433839c3d12412831696a801134ea1
Ted Sazon <tedsazon@gmail.com>
2018-11-21 17:44:00 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,11 @@
+function forLoop(array) {
+  for (let n = 0; n < 25; n++) {
+    if (n === 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push("I am ${n} strange loops.")
+    }
+  }
+    }
+  }
+}
