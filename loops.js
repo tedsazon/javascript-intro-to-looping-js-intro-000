84294e6ee20056d32ccc16ce607fdaa030e04661
Ted Sazon <tedsazon@gmail.com>
2018-11-21 17:57:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -14,3 +14,14 @@ function whileLoop(n) {
   }
   return "done"
 }
+
+function doWhileLoop(array){
+  var i = 0
+  function incrementVariable() {
+    i = i + 1
+  }
+  do { 
+  console.log('array.length = ' + array.length + ' and i = ' + i)
+  array = array.slice(1)}
+} while (array.length > 0 && i < 5)
+  return array
