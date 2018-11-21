e6c43ac7f92e0271ccb7dcf92f4f099042ab050c
Ted Sazon <tedsazon@gmail.com>
2018-11-21 17:46:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,9 @@
 function forLoop(array) {
-  for (let n = 0; n < 25; n++) {
+  for (let n = 0; n < 2; n++) {
     if (n === 1) {
-      array.push("I am 1 strange loop.")
-    } else {
-      array.push("I am ${n} strange loops.")
+      array.push('I am 1 strange loop.')
+    }else {
+        array.push('I am ${n} strange loops.')
     }
-    return array
-  }
-    }
-  }
+  } return array
 }
